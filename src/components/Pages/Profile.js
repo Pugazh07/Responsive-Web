df85d20d56ec34510029b0e54b3ce5e0863df452
Pugazh <pugazh9296@gmail.com>
2021-09-16 00:18:10 +0530

    fixed UseState

--- src/components/Pages/Profile.js
+++ src/components/Pages/Profile.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react';
-import { useState } from 'react/cjs/react.development';
+import React, { useEffect, useState } from 'react';
 
 import ProfileImage from '../../assets/images/profile.png';
 import { Chevron } from '../../assets/vectors/Icons';
